45b6ad89a0345086dc43e785984ef7b5c2a7e748
motobi5 <motobi@naver.com>
2022-12-01 21:50:40 +0900

    221201 webRenewal_update-01

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
 console.clear();
+
 $("#fullpage").fullpage({
   menu: ".line"
 });
+
+
+function SliderBox1__init() {
+
+  const swiper = new Swiper(".slider-box-1 .swiper", {
+    // Optional parameters
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box-1 .swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box-1 .swiper-button-next",
+      prevEl: ".slider-box-1 .swiper-button-prev"
+    }
+  });
+  
+  swiper.on("slideChange", function () {
+    
+    console.log(swiper.activeIndex);
+    $(".content-box").removeClass("active");
+    $(".content-box").eq(swiper.activeIndex).addClass("active");
+    
+  });
+  
+  
+  
+}
+
+SliderBox1__init();
+
