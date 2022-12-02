e34feba906cb31660a27c01a89a46021e3d69a9c
motobi5 <motobi@naver.com>
2022-12-02 11:19:19 +0900

    221202 webRenewal_update-02 appRedesign_update-01(공통슬라이드 커스텀 균일화/앱리디자인페이지생성)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 console.clear();
 
+// 어바웃미
 $("#fullpage").fullpage({
   menu: ".line"
 });
 
-
+// 웹리뉴얼
 function SliderBox1__init() {
 
   const swiper = new Swiper(".slider-box-1 .swiper", {
@@ -34,6 +35,36 @@ function SliderBox1__init() {
   
   
 }
-
 SliderBox1__init();
 
+// 앱리디자인
+function SliderBox2__init() {
+
+  const swiper = new Swiper(".slider-box-2 .swiper", {
+    // Optional parameters
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box-2 .swiper-pagination"
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box-2 .swiper-button-next",
+      prevEl: ".slider-box-2 .swiper-button-prev"
+    }
+  });
+  
+  swiper.on("slideChange", function () {
+    
+    console.log(swiper.activeIndex);
+    $(".content-box").removeClass("active");
+    $(".content-box").eq(swiper.activeIndex).addClass("active");
+    
+  });
+  
+  
+  
+}
+SliderBox2__init();
