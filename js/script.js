d50887625f227423e9418f3724c66326fddfbbef
motobi5 <motobi@naver.com>
2022-12-03 13:05:43 +0900

    221203 aboutMe_update-04(전체적인 레이아웃 간격) / cloneCoding_update-02(버전생성)

--- js/script.js
+++ js/script.js
@@ -104,3 +104,41 @@ function SliderBox3__init() {
 }
 
 SliderBox3__init();
+
+
+
+// 선생님 소스
+// function SliderBox3__init() {
+//   const swiper = new Swiper(".slider-box-3 .swiper", {
+//     // Optional parameters
+//     // loop: true,
+    
+//     // 한 화면에 보일 슬라이드 개수
+//     slidesPerView: 3,
+    
+//     // 활성화된 슬라이드 가운데 정렬 여부
+//     centeredSlides: true,
+//     spaceBetween:-200,
+
+//     // If we need pagination
+//     pagination: {
+//       el: ".slider-box-3 .swiper-pagination"
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: ".slider-box-3 .swiper-button-next",
+//       prevEl: ".slider-box-3 .swiper-button-prev"
+//     }
+//   });
+
+//   swiper.on("slideChange", function () {
+    
+//     console.log(swiper.activeIndex);
+//     $(".slider-box-3 .content-box").removeClass("active");
+//     $(".slider-box-3 .content-box").eq(swiper.activeIndex).addClass("active");
+    
+//   });
+// }
+
+// SliderBox3__init();
