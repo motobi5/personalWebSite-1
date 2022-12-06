ad412e0989042746f5e5910084e1993298bb3670
motobi5 <motobi@naver.com>
2022-12-06 13:09:33 +0900

    221206 home_update-04(인트로 바텀- 애니메이션효과)

--- js/script.js
+++ js/script.js
@@ -1,9 +1,110 @@
 console.clear();
 
+// 눈 내리는 효과
+// (function () {
+
+//   var COUNT = 300;
+//   var masthead = document.querySelector('.sky');
+//   var canvas = document.createElement('canvas');
+//   var ctx = canvas.getContext('2d');
+//   var width = masthead.clientWidth;
+//   var height = masthead.clientHeight;
+//   var i = 0;
+//   var active = false;
+
+//   function onResize() {
+//     width = masthead.clientWidth;
+//     height = masthead.clientHeight;
+//     canvas.width = width;
+//     canvas.height = height;
+//     ctx.fillStyle = '#FFF';
+
+//     var wasActive = active;
+//     active = width > 600;
+
+//     if (!wasActive && active)
+//       requestAnimFrame(update);
+//   }
+
+//   var Snowflake = function () {
+//     this.x = 0;
+//     this.y = 0;
+//     this.vy = 0;
+//     this.vx = 0;
+//     this.r = 0;
+
+//     this.reset();
+//   }
+
+//   Snowflake.prototype.reset = function() {
+//     this.x = Math.random() * width;
+//     this.y = Math.random() * -height;
+//     this.vy = 1 + Math.random() * 3;
+//     this.vx = 0.5 - Math.random();
+//     this.r = 1 + Math.random() * 2;
+//     this.o = 0.5 + Math.random() * 0.5;
+//   }
+
+//   canvas.style.position = 'absolute';
+//   canvas.style.left = canvas.style.top = '0';
+
+//   var snowflakes = [], snowflake;
+//   for (i = 0; i < COUNT; i++) {
+//     snowflake = new Snowflake();
+//     snowflake.reset();
+//     snowflakes.push(snowflake);
+//   }
+  
+//   function update() {
+
+//     ctx.clearRect(0, 0, width, height);
+
+//     if (!active)
+//       return;
+
+//     for (i = 0; i < COUNT; i++) {
+//       snowflake = snowflakes[i];
+//       snowflake.y += snowflake.vy;
+//       snowflake.x += snowflake.vx;
+
+//       ctx.globalAlpha = snowflake.o;
+//       ctx.beginPath();
+//       ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+//       ctx.closePath();
+//       ctx.fill();
+
+//       if (snowflake.y > height) {
+//         snowflake.reset();
+//       }
+//     }
+
+//     requestAnimFrame(update);
+//   }
+
+//   // shim layer with setTimeout fallback
+//   window.requestAnimFrame = (function(){
+//     return  window.requestAnimationFrame       ||
+//             window.webkitRequestAnimationFrame ||
+//             window.mozRequestAnimationFrame    ||
+//             function( callback ){
+//               window.setTimeout(callback, 1000 / 60);
+//             };
+//   })();
+
+//   onResize();
+//   window.addEventListener('resize', onResize, false);
+
+//   masthead.appendChild(canvas);
+// })();
+
 // 홈 로딩페이지
+// 페이지 열릴때 반응하라
+window.onload = function () {
+$(".intro_bottom div").addClass("active");
+}
 setTimeout(function() {
   $('.loading-page').addClass('hide');
-}, 15000);
+}, 14000);
 
 // 홈 인트로 글자 타이핑
 $(document).ready(function () {
@@ -59,6 +160,8 @@ $("#fullpage").fullpage({
 });
 
 // 웹리뉴얼
+
+
 function SliderBox1__init() {
 
   const swiper = new Swiper(".slider-box-1 .swiper", {
