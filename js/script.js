67b3d6956e3c5a6190a24150f87808e9ef4815d4
motobi5 <motobi@naver.com>
2022-12-06 10:17:04 +0900

    221206 home_update-03(인트로생성 및 타이핑효과)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,58 @@
 console.clear();
 
+// 홈 로딩페이지
+setTimeout(function() {
+  $('.loading-page').addClass('hide');
+}, 15000);
+
+// 홈 인트로 글자 타이핑
+$(document).ready(function () {
+  var typingBool = false;
+  var typingIdx = 0;
+  var liIndex = 0;
+  var liLength = $(".typing-txt>ul>li").length;
+
+  // 타이핑될 텍스트를 가져온다
+  var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+  typingTxt = typingTxt.split(""); // 한글자씩 자른다.
+  if (typingBool == false) {
+    // 타이핑이 진행되지 않았다면
+    typingBool = true;
+    var tyInt = setInterval(typing, 100); // 반복동작
+  }
+
+  function typing() {
+    $(".typing ul li").removeClass("on");
+    $(".typing ul li").eq(liIndex).addClass("on");
+    if (typingIdx < typingTxt.length) {
+      // 타이핑될 텍스트 길이만큼 반복
+      $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다.
+      typingIdx++;
+    } else {
+      if (liIndex < liLength - 1) {
+        //다음문장으로  가기위해 인덱스를 1증가
+        liIndex++;
+        //다음문장을 타이핑하기위한 셋팅
+        typingIdx = 0;
+        typingBool = false;
+        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+
+        //다음문장 타이핑전 1초 쉰다
+        clearInterval(tyInt);
+        //타이핑종료
+
+        setTimeout(function () {
+          //1초후에 다시 타이핑 반복 시작
+          tyInt = setInterval(typing, 100);
+        }, 1000);
+      } else if (liIndex == liLength - 1) {
+        //마지막 문장까지 써지면 반복종료
+        clearInterval(tyInt);
+      }
+    }
+  }
+});
+
 // 어바웃미
 $("#fullpage").fullpage({
   menu: ".line"
@@ -80,7 +133,7 @@ function SliderBox3__init() {
     // 활성화된 슬라이드 가운데 정렬 여부
     centeredSlides: true,
 
-    spaceBetween:0,
+    spaceBetween:20,
     slidesPerView: 1.4, // 한 화면에 보일 슬라이드 개수, 소수 가능, effect 설정 시 적용 안 됨(coverflow는 가능)
     // If we need pagination
     pagination: {
@@ -117,40 +170,3 @@ function SliderBox3__init() {
 
 SliderBox3__init();
 
-
-
-// 선생님 소스
-// function SliderBox3__init() {
-//   const swiper = new Swiper(".slider-box-3 .swiper", {
-//     // Optional parameters
-//     // loop: true,
-    
-//     // 한 화면에 보일 슬라이드 개수
-//     slidesPerView: 3,
-    
-//     // 활성화된 슬라이드 가운데 정렬 여부
-//     centeredSlides: true,
-//     spaceBetween:-200,
-
-//     // If we need pagination
-//     pagination: {
-//       el: ".slider-box-3 .swiper-pagination"
-//     },
-
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: ".slider-box-3 .swiper-button-next",
-//       prevEl: ".slider-box-3 .swiper-button-prev"
-//     }
-//   });
-
-//   swiper.on("slideChange", function () {
-    
-//     console.log(swiper.activeIndex);
-//     $(".slider-box-3 .content-box").removeClass("active");
-//     $(".slider-box-3 .content-box").eq(swiper.activeIndex).addClass("active");
-    
-//   });
-// }
-
-// SliderBox3__init();
