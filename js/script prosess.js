35a2e7acbc41174426a53a09d7574007f19c2c8d
motobi5 <motobi@naver.com>
2022-12-30 13:15:52 +0900

    221230 burgerking-prosess update-09(sec-5 6 7 8 9 10 11 12 13 14 애니메이션 효과 생성 /footer 작업/일부코드정리)

--- js/script prosess.js
+++ js/script prosess.js
@@ -5,36 +5,99 @@ AOS.init();
 // 프로세스페이지
 // 페이지 인트로
 $("body").imagesLoaded(function () {
-  setTimeout(function() {
-    $('.prosess-1 > .loading-1').addClass('hide');
+  setTimeout(function () {
+    $(".prosess-1 > .loading-1").addClass("hide");
   }, 3000);
 });
 
 // 비주얼페이지 애니메이션
 window.onload = function () {
-  $('.prosess-1 > .section-1 > .inner > .txt-box').addClass('active');
-  $('.prosess-1 > .section-1 > .inner .date-txt').addClass('active');
-  $('.prosess-1 > .section-1 > .inner .bottom-box > .img-box').addClass('active');
-  }
+  $(".prosess-1 > .section-1 > .inner > .txt-box").addClass("active");
+  $(".prosess-1 > .section-1 > .inner .date-txt").addClass("active");
+  $(".prosess-1 > .section-1 > .inner .bottom-box > .img-box").addClass(
+    "active"
+  );
+};
 
-  // 섹션4 애니메이션
+// 섹션4,6,8,10,12,14 애니메이션
 function Scroll01__init() {
-  $(function() {
-    $(window).on("scroll", function() {
-        if($(window).scrollTop() > 2600) {
-            $(".prosess-1 > .section-4 .color-box").addClass("active");
-            $(".prosess-1 > .section-4 .apphome .title-txt").addClass("active");
-            $(".prosess-1 > .section-4 .inner > .img-box").addClass("active");
-            $(".prosess-1 > .section-4 .right-txt").addClass("active");
-            $(".prosess-1 > .section-4 .bottom-txt").addClass("active");
-            
-        }else {
-           $(".prosess-1 > .section-4 .color-box").removeClass("active");
-           $(".prosess-1 > .section-4 .apphome .title-txt").removeClass("active");
-           $(".prosess-1 > .section-4 .inner > .img-box").removeClass("active");
-           $(".prosess-1 > .section-4 .right-txt").removeClass("active");
-           $(".prosess-1 > .section-4 .bottom-txt").removeClass("active");
-        }
+  $(function () {
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > 2600 && $(window).scrollTop() <= 5200) {
+        $(".prosess-1 > .section-4 .color-box").addClass("active");
+        $(".prosess-1 > .section-4 .apphome .title-txt").addClass("active");
+        $(".prosess-1 > .section-4 .inner > .img-box").addClass("active");
+        $(".prosess-1 > .section-4 .right-txt").addClass("active");
+        $(".prosess-1 > .section-4 .bottom-txt").addClass("active");
+      } else if (
+        $(window).scrollTop() > 5200 && $(window).scrollTop() <= 7200
+      ) {
+        $(".prosess-1 > .section-6 .color-box").addClass("active");
+        $(".prosess-1 > .section-6 .appsearch .title-txt").addClass("active");
+        $(".prosess-1 > .section-6 .inner > .img-box").addClass("active");
+        $(".prosess-1 > .section-6 .right-txt").addClass("active");
+        $(".prosess-1 > .section-6 .left-txt").addClass("active");
+      } else if (
+        $(window).scrollTop() > 7200 && $(window).scrollTop() <= 8600
+      ) {
+        $(".prosess-1 > .section-8 .color-box").addClass("active");
+        $(".prosess-1 > .section-8 .apporder .title-txt").addClass("active");
+        $(".prosess-1 > .section-8 .inner > .img-box").addClass("active");
+        $(".prosess-1 > .section-8 .right-txt").addClass("active");
+      }else if (
+        $(window).scrollTop() > 8600 && $(window).scrollTop() <= 10000
+      ) {
+        $(".prosess-1 > .section-10 .color-box").addClass("active");
+        $(".prosess-1 > .section-10 .appOrderCheck .title-txt").addClass("active");
+        $(".prosess-1 > .section-10 .inner > .img-box").addClass("active");
+        $(".prosess-1 > .section-10 .left-txt").addClass("active");
+      }else if (
+        $(window).scrollTop() > 10000 && $(window).scrollTop() <= 12500
+      ) {
+        $(".prosess-1 > .section-12 .color-box").addClass("active");
+        $(".prosess-1 > .section-12 .appPayment .title-txt").addClass("active");
+        $(".prosess-1 > .section-12 .inner > .img-box").addClass("active");
+        $(".prosess-1 > .section-12 .right-txt").addClass("active");
+      }else if (
+        $(window).scrollTop() > 12500 && $(window).scrollTop() <= 14000
+      ) {
+        $(".prosess-1 > .section-14").addClass("active");
+        $(".prosess-1 > .section-14 .thx-txt").addClass("active");
+      }
+      // else if (
+      //   $(window).scrollTop() > 13400 && $(window).scrollTop() <= 14000
+      // ) {
+      //   // alert("여기는 푸터");
+      //   $(".prosess-1 > .footer > .inner > div").addClass("active");
+      // }
+      else {
+        $(".prosess-1 > .section-4 .color-box").removeClass("active");
+        $(".prosess-1 > .section-4 .apphome .title-txt").removeClass("active");
+        $(".prosess-1 > .section-4 .inner > .img-box").removeClass("active");
+        $(".prosess-1 > .section-4 .right-txt").removeClass("active");
+        $(".prosess-1 > .section-4 .bottom-txt").removeClass("active");
+        $(".prosess-1 > .section-6 .color-box").removeClass("active");
+        $(".prosess-1 > .section-6 .appsearch .title-txt").removeClass(
+          "active");
+        $(".prosess-1 > .section-6 .inner > .img-box").removeClass("active");
+        $(".prosess-1 > .section-6 .left-txt").removeClass("active");
+        $(".prosess-1 > .section-6 .right-txt").removeClass("active");
+        $(".prosess-1 > .section-8 .color-box").removeClass("active");
+        $(".prosess-1 > .section-8 .apporder .title-txt").removeClass("active");
+        $(".prosess-1 > .section-8 .inner > .img-box").removeClass("active");
+        $(".prosess-1 > .section-8 .right-txt").removeClass("active");
+        $(".prosess-1 > .section-10 .color-box").removeClass("active");
+        $(".prosess-1 > .section-10 .appOrderCheck .title-txt").removeClass("active");
+        $(".prosess-1 > .section-10 .inner > .img-box").removeClass("active");
+        $(".prosess-1 > .section-10 .left-txt").removeClass("active");
+        $(".prosess-1 > .section-12 .color-box").removeClass("active");
+        $(".prosess-1 > .section-12 .appPayment .title-txt").removeClass("active");
+        $(".prosess-1 > .section-12 .inner > .img-box").removeClass("active");
+        $(".prosess-1 > .section-12 .right-txt").removeClass("active");
+        $(".prosess-1 > .section-14").removeClass("active");
+        $(".prosess-1 > .section-14 .thx-txt").removeClass("active");
+        // $(".prosess-1 > .footer > .inner > div").removeClass("active");
+      }
     });
     // $(".top").click(function(){
     //   $("html, body").animate({scrollTop:0}, 800);
@@ -49,7 +112,6 @@ function Scroll01__init() {
     //     $(".top.__footer-bottom").css("bottom", "60px");
     //   }
     // });
-    });
-    }
-
+  });
+}
 Scroll01__init();
