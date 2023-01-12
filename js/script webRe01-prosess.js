e0435c8ee458ed0bb83be46f87b8ed21bac1ac8c
motobi5 <motobi@naver.com>
2023-01-12 16:40:54 +0900

    230112 code_allcheck-12 (webRe01-prose js파일 분리)

--- js/script webRe01-prosess.js
+++ js/script webRe01-prosess.js
@@ -73,30 +73,4 @@ function Scroll02__init() {
   });
 }
 
-function QuickMenuScroll__init() {
-  $(function () {
-    $(window).on("scroll", function () {
-      if ($(window).scrollTop() > 400) {
-        $(".web_prosess_1  > .footer .top").addClass("show");
-      } else {
-        $(".web_prosess_1  > .footer .top").removeClass("show");
-      }
-    });
-    $(".web_prosess_1  > .footer .top").click(function () {
-      $("html, body").animate({ scrollTop: 0 }, 800);
-      return false;
-    });
-    // 스크롤 맨 아래라고 감지했을 때 버튼 위치 이동
-    $(window).scroll(function () {
-      if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-        $(".web_prosess_1  > .footer .top.__footer-bottom").css("bottom", "248px");
-      } else {
-        $(".web_prosess_1  > .footer .top.__footer-bottom").css("bottom", "60px");
-      }
-    });
-  });
-}
-
-
 Scroll02__init();
-QuickMenuScroll__init();
---
+++ js/script webRe01-quick.js
@@ -0,0 +1,27 @@
+console.clear();
+
+
+function QuickMenuScroll__init() {
+  $(function () {
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > 600) {
+        $(".web_prosess_1  > .footer .top").addClass("show");
+      } else {
+        $(".web_prosess_1  > .footer .top").removeClass("show");
+      }
+    });
+    $(".web_prosess_1  > .footer .top").click(function () {
+      $("html, body").animate({ scrollTop: 0 }, 800);
+      return false;
+    });
+    // 스크롤 맨 아래라고 감지했을 때 버튼 위치 이동
+    $(window).scroll(function () {
+      if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $(".web_prosess_1  > .footer .top.__footer-bottom").css("bottom", "248px");
+      } else {
+        $(".web_prosess_1  > .footer .top.__footer-bottom").css("bottom", "60px");
+      }
+    });
+  });
+}
+QuickMenuScroll__init();
