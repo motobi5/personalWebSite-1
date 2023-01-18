59763242e4b7a75ba97b06680e9a879c0078e3bc
motobi5 <motobi@naver.com>
2023-01-18 21:37:23 +0900

    230118 webRe01-proce update-22(퀵버튼 자바스크립트 추가

--- js/script webRe01-process.js
+++ js/script webRe01-process.js
@@ -74,3 +74,30 @@ function Scroll02__init() {
 }
 
 Scroll02__init();
+
+function QuickMenuScroll__init() {
+  $(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 400) {
+            $(".web_process_1 > .footer .top").addClass("show");
+         
+        } else {
+           $(".web_process_1 > .footer .top").addClass("show");
+        }
+    });
+    $(".web_process_1 > .footer .top").click(function(){
+      $("html, body").animate({scrollTop:0}, 800);
+      return false;
+    });
+    // 스크롤 맨 아래라고 감지했을 때 버튼 위치 이동
+    $(window).scroll(function () {
+      if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $(".web_process_1 > .footer .top.__footer-bottom").css("bottom", "248px");
+      } else {
+        $(".web_process_1  > .footer .top.__footer-bottom").css("bottom", "60px");
+      }
+    });
+  });
+    }
+
+QuickMenuScroll__init();
